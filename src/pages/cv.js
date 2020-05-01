683d9d7da213e5d97be988b80162f9e286cf83ea
Marcell Endrey <marcell.endrey@prezi.com>
2020-05-01 13:20:04 +0200

    feat: add some images using gatsby-image

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,16 +1,27 @@
 import React from "react"
+import {graphql} from "gatsby"
+import Img from "gatsby-image"
 import {Layout} from "../components/layout"
 import style from "./cv.module.css"
 
-const ProfilePicture = (
-  <img
-    className={style.circularCutout}
-    src="https://static.wixstatic.com/media/177945_6a2a580c4fc9469a995475e74eda7183~mv2_d_3456_3888_s_4_2.jpg/v1/crop/x_0,y_46,w_3456,h_3350/fill/w_450,h_436,al_c,q_80,usm_0.66_1.00_0.01/psjobb.webp"
-  />
+const ProfilePicture = ({data}) => (
+  <Img className={style.circularCutout} fluid={data.file.childImageSharp.fluid} alt="Kádár Emese profile picture" />
 )
 
-export default () => (
-  <Layout leftSidebarRender={ProfilePicture}>
+export default ({data}) => (
+  <Layout leftSidebarRender={<ProfilePicture data={data} />}>
     <div>CV comes here</div>
   </Layout>
 )
+
+export const query = graphql`
+  query ProfilePictureQuery {
+    file(relativePath: {eq: "cv/profile.webp"}) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,22 @@
 import React from "react"
+import {graphql} from "gatsby"
+import Img from "gatsby-image"
 import {Layout} from "../components/layout"
 
-export default () => (
+export default ({data}) => (
   <Layout>
-    <img src="https://static.wixstatic.com/media/177945_dbcdce52d2aa464fbefa97ed23a21936~mv2_d_4560_2952_s_4_2.jpg/v1/fill/w_1514,h_966,al_c,q_85,usm_0.66_1.00_0.01/177945_dbcdce52d2aa464fbefa97ed23a21936~mv2_d_4560_2952_s_4_2.webp" />
+    <Img fluid={data.file.childImageSharp.fluid} alt="Őrlődés" />
   </Layout>
 )
+
+export const query = graphql`
+  query IndexCarouselQuery {
+    file(relativePath: {eq: "index/carousel-1.webp"}) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
