e6068de4cb7e604487dbccb1a145bcb97bed62b0
Marcell Endrey <marcell.endrey@prezi.com>
2020-05-01 15:43:07 +0200

    feat: implement home page image slideshow

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,38 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import {graphql} from "gatsby"
 import Img from "gatsby-image"
 import {Layout} from "../components/layout"
 
-export default ({data}) => (
-  <Layout>
-    <Img fluid={data.file.childImageSharp.fluid} alt="Őrlődés" />
-  </Layout>
-)
+export default ({data}) => {
+  const [index, setIndex] = useState(0)
+  const newIndex = (index + 1) % data.allFile.edges.length
+  useEffect(() => {
+    setTimeout(() => {
+      setIndex(newIndex)
+    }, 5000)
+  }, [index])
+  const image = data.allFile.edges[index]
+  return (
+    <Layout>
+      <Img
+        fluid={image.node.childImageSharp.fluid}
+        alt={image.node.base.split(".")[0]} // only use section of the file extension with the filename
+      />
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query IndexCarouselQuery {
-    file(relativePath: {eq: "index/carousel-1.webp"}) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid
+    allFile(filter: {relativeDirectory: {eq: "index"}}) {
+      edges {
+        node {
+          base
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
         }
       }
     }
