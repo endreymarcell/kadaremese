4591fa9019211a0dab779ed079b18ec42ecb68df
Marcell Endrey <marcell.endrey@prezi.com>
2020-05-01 12:49:49 +0200

    feat: implement sidebar navigation logic

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,32 +1,107 @@
-import React from "react"
+import React, {useState} from "react"
 import {Link} from "gatsby"
 import style from "./sidebar.module.css"
 
-export const Sidebar = () => (
-  <div className={style.sidebar}>
+const SubNavInstallaciok = () => {
+  return (
     <div>
-      <div className={style.mainNav}>
-        <div>
-          <Link to="/cv" activeClassName="current-page">
-            Rólam
-          </Link>
+      <div>
+        <Link to="/installaciok/betonpixelek" activeClassName="current-page">
+          Betonpixelek
+        </Link>
+      </div>
+      <div>
+        <Link to="/installaciok/privat-horizont" activeClassName="current-page">
+          Privát horizont
+        </Link>
+      </div>
+      <div>
+        <Link to="/installaciok/orlodes" activeClassName="current-page">
+          Őrlődés
+        </Link>
+      </div>
+      <div>
+        <Link to="/installaciok/inkognito-mod" activeClassName="current-page">
+          Inkognito mód
+        </Link>
+      </div>
+      <div>
+        <Link to="/installaciok/no-line" activeClassName="current-page">
+          No-line
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+const SubNavFestmenyek = () => {
+  return (
+    <div>
+      <div>
+        <Link to="/festmenyek/2014-15-ruhak" activeClassName="current-page">
+          2014-15 Ruhák
+        </Link>
+      </div>
+      <div>
+        <Link to="/festmenyek/2015-hungarikumok" activeClassName="current-page">
+          2015 Hungarikumok
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+const getSubNav = (subNav) => {
+  switch (subNav) {
+    case "installaciok":
+      return () => <SubNavInstallaciok />
+    case "festmenyek":
+      return () => <SubNavFestmenyek />
+    default:
+      return () => <div />
+  }
+}
+
+export const Sidebar = () => {
+  const [subNavState, setSubNavState] = useState(null)
+  const SubNavElement = getSubNav(subNavState)
+  return (
+    <div className={style.sidebar} onMouseLeave={() => setSubNavState(null)}>
+      <div>
+        <div className={style.mainNav}>
+          <div>
+            <Link to="/cv" activeClassName="current-page">
+              Rólam
+            </Link>
+          </div>
+          <div>
+            <Link
+              to="/installaciok/betonpixelek"
+              activeClassName="current-page"
+              onMouseEnter={() => setSubNavState("installaciok")}
+            >
+              Installációk
+            </Link>
+          </div>
+          <div>
+            <Link
+              to="/festmenyek/2014-15-ruhak"
+              activeClassName="current-page"
+              onMouseEnter={() => setSubNavState("festmenyek")}
+            >
+              Festmények
+            </Link>
+          </div>
         </div>
+      </div>
+      <div>
+        <Link to="/" className={style.siteTitle}>
+          Kádár Emese
+        </Link>
         <div>
-          <Link to="/installaciok/betonpixelek" activeClassName="current-page">
-            Installációk
-          </Link>
-        </div>
-        <div>
-          <Link to="/festmenyek" activeClassName="current-page">
-            Festmények
-          </Link>
+          <SubNavElement />
         </div>
       </div>
     </div>
-    <div>
-      <Link to="/" className={style.siteTitle}>
-        Kádár Emese
-      </Link>
-    </div>
-  </div>
-)
+  )
+}
