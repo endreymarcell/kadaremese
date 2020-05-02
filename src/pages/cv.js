ec99e5a5d7cf8be898b3daae06e71dbc22cf3883
Marcell Endrey <marcell.endrey@prezi.com>
2020-05-02 16:40:49 +0200

    fix: keep styles local to the CV

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -2,8 +2,16 @@ import React from "react"
 import {graphql} from "gatsby"
 import Img from "gatsby-image"
 import {documentToReactComponents} from "@contentful/rich-text-react-renderer"
+import {BLOCKS} from "@contentful/rich-text-types"
 import {Layout} from "../components/layout"
 import style from "./cv.module.css"
+
+const options = {
+  renderNode: {
+    [BLOCKS.HEADING_1]: (node, children) => <h1 className={style.sectionHeader}>{children}</h1>,
+    [BLOCKS.HEADING_2]: (node, children) => <h2 className={style.subsectionHeader}>{children}</h2>,
+  },
+}
 
 export default ({data}) => (
   <Layout>
@@ -11,7 +19,9 @@ export default ({data}) => (
       <div className={style.profilePictureContainer}>
         <Img className={style.circularCutout} fluid={data.contentfulAsset.fluid} alt="Kádár Emese profile picture" />
       </div>
-      <div className={style.cvContainer}>{documentToReactComponents(data.contentfulCvPageBodyRichTextNode.json)}</div>
+      <div className={style.cvContainer}>
+        {documentToReactComponents(data.contentfulCvPageBodyRichTextNode.json, options)}
+      </div>
     </div>
   </Layout>
 )
