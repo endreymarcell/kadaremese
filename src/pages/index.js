e52dfdb5b85dfcdaf1cf2f3cd404bd5075c8365b
Marcell Endrey <marcell.endrey@prezi.com>
2020-05-02 16:01:59 +0200

    chore: load index slideshow from contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import style from "./index.module.css"
 
 export default ({data}) => {
   const [index, setIndex] = useState(0)
-  const newIndex = (index + 1) % data.allFile.edges.length
+  const newIndex = (index + 1) % data.contentfulHomepageSlideshow.images.length
   useEffect(() => {
     setTimeout(() => {
       setIndex(newIndex)
@@ -14,11 +14,11 @@ export default ({data}) => {
   }, [newIndex])
   return (
     <Layout>
-      {data.allFile.edges.map((image, i) => (
+      {data.contentfulHomepageSlideshow.images.map((image, i) => (
         <div className={style.imageContainer} style={index !== i ? {display: "none"} : {}}>
           <Img
-            fluid={image.node.childImageSharp.fluid}
-            alt={image.node.base.split(".")[0]} // only use section of the file extension with the filename
+            fluid={image.fluid}
+            alt={image.title} // only use section of the file extension with the filename
           />
         </div>
       ))}
@@ -27,16 +27,11 @@ export default ({data}) => {
 }
 
 export const query = graphql`
-  query IndexCarouselQuery {
-    allFile(filter: {relativeDirectory: {eq: "index"}}, sort: {fields: base}) {
-      edges {
-        node {
-          base
-          childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid
-            }
-          }
+  query IndexSlideshowQuery {
+    contentfulHomepageSlideshow {
+      images {
+        fluid {
+          ...GatsbyContentfulFluid
         }
       }
     }
