6ab918dd35a062c10bf284be3614cfbfeda56678
Marcell Endrey <marcell.endrey@prezi.com>
2020-08-16 17:35:15 +0200

    feat: ability to disable the homepage slideshow

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import {graphql} from "gatsby"
 import Img from "gatsby-image"
 import {Layout} from "../components/layout"
 import style from "./index.module.css"
+import {shouldStartSlideshow} from "../utils"
 
 export default ({data}) => {
   const {
@@ -11,9 +12,11 @@ export default ({data}) => {
   const [index, setIndex] = useState(0)
   const newIndex = (index + 1) % images.length
   useEffect(() => {
-    setTimeout(() => {
-      setIndex(newIndex)
-    }, interval * 1000)
+    if (shouldStartSlideshow()) {
+      setTimeout(() => {
+        setIndex(newIndex)
+      }, interval * 1000)
+    }
   }, [newIndex, interval])
   return (
     <Layout>
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,6 @@
 export const flatten = (arrays) => [].concat.apply([], arrays)
+
+export const shouldStartSlideshow = () => {
+  const url = new URL(window.location.href)
+  return !url.searchParams.has("no-slideshow")
+}
